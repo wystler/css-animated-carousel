9b0946a9b84eae0bc9400101514fb7f602b2d5bd
wystler <benjamin.rudd@live.com>
2023-02-07 11:10:02 +0000

    add hardcoded 5 slide carousel

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
+
 export const Carousel = () => {
 
     const slides = ["red", "orange", "yellow", "yellowgreen", "green", "cyan", "blue", "purple", "magenta", "pink"]     //10
 
+    const [count, setCount] = useState(0)
+
     return (
         <div className="carousel">
+            {Array(5).fill().map((slide, index) => {
+                return <div 
+                    className="slide"
+                    onAnimationIteration={() => setCount(count + 1)}
+                    style={{
+                        animationDelay:`-${index*1}s`,
+                        backgroundColor:slides[index]
+                    }}
+                    >
+                </div>})}
         </div>
     )
 }
