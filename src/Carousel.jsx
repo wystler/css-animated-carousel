67f035be4ac685bddf83e1527a312cb81d7adf70
wystler <benjamin.rudd@live.com>
2023-02-08 11:31:32 +0000

    rotates through each element of the passed slides array, but in the wrong order

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react"
 import { Slide } from "./Slide.jsx"
 
 export const Carousel = () => {
+
+    const rotationTime = 5
+    const numberOfSlides = 5
 
     return (
         <div className="container">
             <div className="carousel">
                 {Array(5).fill().map((slide, index) => {
-                    return <Slide index={index}/>
+                    return <Slide 
+                        index={index}
+                        rotationTime={rotationTime}
+                        numberOfSlides={numberOfSlides}
+                        />
                 })}
             </div>
         </div>
--- src/Slide.jsx
+++ src/Slide.jsx
@@ -2,18 +2,20 @@ import { useState } from "react"
 
 export const Slide = (props) => {
 
-    const {index} = props
+    const {index, rotationTime, numberOfSlides} = props
     const [count, setCount] = useState(0)
     const slides = ["red", "orange", "yellow", "yellowgreen", "green", "cyan", "blue", "purple", "magenta", "pink"]     //10
+
+    const animationDelay = -index*rotationTime/numberOfSlides
 
     return (
         <div 
         className="slide"
         onAnimationIteration={() => setCount(count + 1)}
         style={{
-            animationDelay:`${index+0.1}s`,
-            animationDuration:`5s`,
-            backgroundColor:slides[(index+(5*count))%10]
+            animationDelay:`${animationDelay}s`,
+            animationDuration:{rotationTime},
+            backgroundColor:slides[(index+(count*numberOfSlides))%slides.length]
         }}
         >
     </div>
